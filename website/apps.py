80ea0c846e7a198aa796876c7ee5f04cd6f27b67
RubanKumarK <ceeeerubankumar513@gmail.com>
2026-07-15 17:11:45 +0530

    Auto create superuser

--- website/apps.py
+++ website/apps.py
@@ -1,5 +1,21 @@
 from django.apps import AppConfig
 
+
 class WebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website'  # <-- this should be the folder name
+    name = 'website'
+
+    def ready(self):
+        import os
+
+        if os.environ.get("CREATE_SUPERUSER") == "True":
+            from django.contrib.auth import get_user_model
+
+            User = get_user_model()
+
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@gmail.com",
+                    password="Admin@123"
+                )

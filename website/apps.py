4fcf6c5eae615c9062c749beccd40ddc7f5b63b0
RubanKumarK <ceeeerubankumar513@gmail.com>
2026-07-15 17:19:44 +0530

    Remove auto superuser

--- website/apps.py
+++ website/apps.py
@@ -1,21 +1,5 @@
 from django.apps import AppConfig
 
-
 class WebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website'
-
-    def ready(self):
-        import os
-
-        if os.environ.get("CREATE_SUPERUSER") == "True":
-            from django.contrib.auth import get_user_model
-
-            User = get_user_model()
-
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    username="admin",
-                    email="admin@gmail.com",
-                    password="Admin@123"
-                )
+    name = 'website'  # <-- this should be the folder name

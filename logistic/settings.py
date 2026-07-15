08802a26fe50130cd0c25acbf8608817a5cd0d8d
RubanKumarK <ceeeerubankumar513@gmail.com>
2026-07-15 22:58:15 +0530

    change order

--- logistic/settings.py
+++ logistic/settings.py
@@ -41,13 +41,14 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     "jazzmin",
-    'colorfield',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'colorfield',
     'website',
     'settings',
     'EmailConfiguration',
